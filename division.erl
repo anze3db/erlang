6beff4c78a844601ea97d189f03009fe3e9fd053
Smotko <ensmotko@gmail.com>
2011-05-19 14:21:44 +0200

    Added division from our Theoretical Foundations of Computer Science class.

---
+++ division.erl
@@ -0,0 +1,21 @@
+-module(division).
+-export([d/2]).
+
+prev(X) -> X - 1.
+
+next(X) -> X + 1.
+
+sub(X,Y) when X < Y -> 0;
+sub(X,Y) -> X - Y.
+
+division(_,0) -> 0;
+division(Y,X) -> 
+	X1 = X - 1,
+	case (sub(next(X1), Y*division(Y,X1))) > 0 of
+	   true ->         
+		next(division(Y,X1));
+	   false ->
+		division(Y,X1)
+	end.
+	   
+d(X,Y) -> prev(division(Y, next(X))).
