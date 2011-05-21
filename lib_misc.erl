6cd93ea473387c4e25cfaf64bd422e6bd3fe435e
Smotko <ensmotko@gmail.com>
2011-05-21 09:51:55 +0200

    Added pythag function

--- lib_misc.erl
+++ lib_misc.erl
@@ -13,4 +13,13 @@ qsort([Pivot|T]) ->
     qsort([X || X <- T, X < Pivot])
     ++ [Pivot] ++
     qsort([X || X <- T, X >= Pivot]).
- 
+
+pythag(N) -> 
+        [{A,B,C} || 
+            A <- lists:seq(1,N),
+            B <- lists:seq(1,N),
+            C <- lists:seq(1,N),
+            A+B+C =< N,    
+            A*A + B*B =:= C*C
+        ].
+
